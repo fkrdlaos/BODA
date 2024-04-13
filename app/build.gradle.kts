b7cef7c2bc994ae3b8327f7449e7b81e7415abda
GH Baek <backtobaek9922@gmail.com>
2024-04-13 16:08:07 +0900

    http get caption 기능 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.monitor)
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/org/techtown/boda/HttpsTask.java
@@ -0,0 +1,55 @@
+package org.techtown.boda;
+
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+
+import java.io.InputStream;
+
+public class HttpsTask extends AsyncTask<Void, Void, String> {
+
+    private final InputStream inputStream;
+
+    // Constructor
+    public HttpsTask(InputStream inputStream) {
+        this.inputStream = inputStream;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        String result; // 요청 결과를 저장할 변수.
+
+//        HttpsGetCaption requestHttpsURLConnection = new HttpsGetCaption(this.imageBitmap);
+//        result = requestHttpsURLConnection.sendPostRequest();
+        result = GetCaption.sendCaptionRequest(inputStream);
+
+        try {
+            if (result != null) {
+                System.out.println("[doInBackground] output : " + result);
+            } else {
+                System.out.println("[doInBackground] output is null state.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+
+        try {
+            if (result != null) {
+                System.out.println(result);
+            } else {
+                System.out.println("[onPostExecute] output is null state.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
